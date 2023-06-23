4e845ed48c542ee16556b88202f24e7c89e6b7c9
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2023-06-23 14:12:51 +0300

    patch: templates

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,5 +2,11 @@ from app import views
 from django.urls import path,include
 
 urlpatterns = [
-    path('',views.index, name='index')
+    
+    path('',views.index, name='index'),
+    path('about', views.about,name='about'),
+    path('contact',views.contact,name='contact'),
 ]
+    
+
+
--- project/app/views.py
+++ project/app/views.py
@@ -7,3 +7,9 @@ def index(request):
     return render(request,'index.html')
 
 
+def about(request):
+    return render(request,'about.html')
+
+
+def contact(request):
+    return render(request,'contact.html')
