a5b448e526209f8ec9be80af4490fae7549675a1
JAREDKEVIN <kevin.kipkemoi@student.moringaschool.com>
2023-07-03 11:18:30 +0300

    create: url path views

--- project/portfolio/urls.py
+++ project/portfolio/urls.py
@@ -4,5 +4,11 @@ from django.urls import path,include
 urlpatterns = [
     
     path('',views.home, name='home'),
+    path('about',views.about, name='about'),
+    path('contact',views.contact, name='contact'),
+    path('resume',views.resume, name='resume'),
+    path('portfolio',views.portfolio, name='portfolio'),
+    path('services',views.services, name='services'),
+    
 
 ]
